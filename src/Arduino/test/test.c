b0f20027402a90f55aff2d03a0413ae75a2a6816
Arnau Prat <arnau@vilynx.com>
2015-03-24 13:38:29 +0100

    improved test for Arduino

--- src/Arduino/test/test.c
+++ src/Arduino/test/test.c
@@ -16,17 +16,49 @@ int main(int argc, char const *argv[])
 	/*
      * Basic I/O
 	 *
-	 *	0	0	1	0	1	1	0	1 <- read digital pin (57)
-	 *	0	0	0	0	0	1	0	1 <- digital pin nº 2  (5)
-	 *	0	0	0	0	0	0	1	0 <- end & packet = 1  (2)
+	 *	0	0	1	0	1	1	0	1 <- read digital pin (57) x2D
+	 *	0	0	0	0	0	1	0	1 <- digital pin nº 2  (5) x05
+	 *	0	0	0	0	0	0	1	0 <- end & packet = 1  (2) x02
 	 *
 	 */
 
+	/*
+     * Basic Event
+	 *
+	 *	0	1	1	0	0	1	0	1 <- set event prescaler 2	(101)	0x65
+	 *	0	0	0	0	0	0	1	1 <- act 1: digitalWrite	  (3) 	0x03
+	 *	0	0	0	1	0	0	0	1 <- opt 1: pin  number		  (8)	0x11
+	 *	0	0	0	0	0	1	0	0 <- end & packet = 2  		  (2) 	0x04
+	 *
+	 */
+
+	//digitalWrite(2,HIGH)
+
 	char packet1[] = "\x2D\x5\x2";
+
+	//digitalWrite(2,LOW)
 
 	char packet2[] = "\x29\x5\x2";
 
+	//Event pre=2 toggle(8)
+
+	char packet3[] = "\x65\x3\x11\x4";
+
+	/* create an event */
+
+	write(fd,&packet3,sizeof(packet3));	
+
+	sleep(5);
+
+	/* create an event */
+
+	write(fd,&packet3,sizeof(packet3));	
+
+	sleep(5);
+
 	while(1) {
+
+	/* blink a led */
 
 	write(fd,&packet1,sizeof(packet1));	
 
