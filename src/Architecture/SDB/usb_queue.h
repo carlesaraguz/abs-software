f167746403f5a2951abc9624bf60341c8e1ad15b
Arnau Prat <arnau@vilynx.com>
2015-03-24 22:20:33 +0100

    sdb_queue corrected

--- src/Architecture/SDB/usb_queue.h
+++ src/Architecture/SDB/usb_queue.h
@@ -1,34 +1,33 @@
-#ifndef USB_QUEUE_H
-#define USB_QUEUE_H
+#ifndef __USB_QUEUE_H
+#define __USB_QUEUE_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-#include "sdb_dummy.h" //sdb.h
+#define USB_QUEUE_SIZE 128
 
-#define MAX_SIZE 128
+typedef struct QueueElement { 
+    void * data; 
+    int priority; 
+} QueueElement;
 
-typedef struct { 
-	void * data; 
-	int priority; 
-} QueueElem;
+typedef struct USBQueue { 
+    QueueElement *buf; 
+    int n, alloc; 
+} USBQueue;
 
-typedef struct { 
-	QueueElem *buf; 
-	int n, alloc; 
-} *UsbQueue, UsbQueueT;
-
-extern UsbQueue queue;
+extern USBQueue *queue;
 
 extern pthread_mutex_t usb_queue_lock;
 
 extern sem_t packet_queue_count;
 
-void usb_queue_init();
+void usb_queue_init(void);
 
 void usb_queue_push(void *data, int pri);
 
 void *usb_queue_pop(int *pri);
 
-#endif /* USB_QUEUE_H */
+#endif /* __USB_QUEUE_H */
